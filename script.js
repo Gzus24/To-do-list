61347db157b6cbded57ccc469eaec2abbdd846c7
Gzus24 <jromero3418@gmail.com>
2023-07-07 12:36:16 -0700

    to do list project

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let input = document.querySelector('input');
+const btn = document.querySelector('.add');
+let ul = document.createElement('ul');
+let section = document.querySelector('section');
+section.appendChild(ul);
+ul.classList.add('ul')
+
+btn.addEventListener('click', addList);
+
+function addList(){
+    const inputVal = document.querySelector('.input').value;
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode('poop'))
+    ul.appendChild(li);
+    li.classList.add('li');
+    li.textContent = inputVal;
+    let odd = document.querySelectorAll('.li:nth-child(odd)');
+    odd.forEach(e => e.style.background = '#001C30');
+        li.onclick =()=> {
+            ul.removeChild(li);
+         }
+}
