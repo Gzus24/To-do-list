3fbea3e35c6b770d853e6f10d9db743248ddacf6
Gzus24 <jromero3418@gmail.com>
2023-07-28 13:20:13 -0700

    changes to script.js

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ const ul = document.createElement('ul');
 const section = document.querySelector('section');
 section.appendChild(ul);
 
-function ToDo (chore){
-    this.chore = chore;
-
-    this.setTask = function(){
-        const li = document.createElement('li');
+const toDo = {
+    setTask: function(chore){
+        let li = document.createElement('li');
         ul.appendChild(li);
         li.textContent = chore;
-        li.onclick =()=>{
-            ul.removeChild(li);
-        }
-    };
-    this.setOddColor = function(){
-        let odd = document.querySelectorAll('li:nth-child(odd)');
-        odd.forEach(e => e.style.background = '#001C30')
+            li.onclick = ()=>{
+                ul.replaceChild(li);
+            }
+    },
+    setOddColor: function(){
+        let oddLi = document.querySelectorAll('li:nth-child(odd)');
+        oddLi.forEach(e => e.style.background = '#001C30')
     }
 }
 
 btn.onclick = ()=>{
-    const inputVal = document.querySelector('.input').value;
-    let task = new ToDo(inputVal);
-    task.setTask();
-    task.setOddColor();
+    const inputVal = input.value;
+    toDo.setTask(inputVal);
+    toDo.setOddColor();
 }
